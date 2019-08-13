a04d81843d2584bbc7050ee61ab1f199060f0d3c
phajvaj <saciw.doi@gmail.com>
2019-08-13 12:47:56 +0700

    update api for status interview

--- queue-api/src/routes/api.ts
+++ queue-api/src/routes/api.ts
@@ -685,7 +685,10 @@ const router = (fastify, { }, next) => {
               await queueModel.removeCurrentQueue(db, servicePointId, dateServ, queueId);
               await queueModel.updateCurrentQueue(db, servicePointId, dateServ, queueId, roomId);
               // await queueModel.markUnPending(db, queueId);
-              await queueModel.markCompleted2(db, queueId, isInterview);
+              //let state = isInterview == 'Y' ? 'N' : 'Y';
+              if (isInterview == 'N')
+                await queueModel.markCompleted(db, queueId);
+
               var _queueIds: any = [];
               _queueIds.push(queueId);
 
